14f9ba1b7917e1a125fec60446a569a3792c88db
Artem Agafonov <agart@mail.ru>
2023-10-12 21:16:51 +0300

    rggo. Chapter 3. Finish mdp version 1.

--- rggo/03.workingFiles/01.mdp/main_test.go
+++ rggo/03.workingFiles/01.mdp/main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io/ioutil" // To read data from files
+	"os"
 	"testing"
 )
 
@@ -28,9 +29,55 @@ func TestParseContent(t *testing.T) {
 	actual := parseContent(input)
 
 	// Assert
-	if !bytes.Equal(expected, actual) {
-		t.Logf("expected:\n%s\n", expected)
-		t.Logf("result:\n%s\n", actual)
-		t.Error("Actual content does not match expected")
+	assertBytes(expected, actual, t)
+}
+
+func TestRun(t *testing.T) {
+	// Act
+	if err := run(inputFile); err != nil {
+		t.Fatal(err)
 	}
+
+	exists, err := exists(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		defer os.Remove(resultFile)
+	}
+
+	actual, err := ioutil.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Assert
+	assertBytes(expected, actual, t)
+}
+
+func assertBytes(expected []byte, actual []byte, t *testing.T) {
+	if bytes.Equal(expected, actual) {
+		return
+	}
+
+	t.Logf("expected:\n%s\n", expected)
+	t.Logf("result:\n%s\n", actual)
+	t.Error("Actual content does not match expected")
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
 }
