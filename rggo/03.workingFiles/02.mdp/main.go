d344308614fdeb625b82867c4e4b0e2eea616ae4
Artem Agafonov <agart@mail.ru>
2023-10-12 22:15:51 +0300

    rggo. Chapter 3. Add mdp version 2.

---
+++ rggo/03.workingFiles/02.mdp/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io" // To use io.Writer interface
+	"io/ioutil"
+	"os"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	header = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Markdown Preview Tool</title>
+  </head>
+  <body>
+`
+	footer = `
+  </body>
+</html>
+`
+	// File permissions.
+	// Readable and writable by the owner and only readable by anyone else.
+	filemode = 0644
+)
+
+func main() {
+	// Parse flags
+	filename := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	// If user did not provide input file, show usage
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string, out io.Writer) error {
+	// Read all data from the input and check for errors
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData := parseContent(input)
+
+	// Create temporary file and check for errors
+	// "" - system-defined temporary directory
+	// "mdp*.html" - filename pattern
+	temp, err := ioutil.TempFile("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
+
+	outFilename := temp.Name()
+	fmt.Fprintln(out, outFilename)
+
+	return saveHtml(outFilename, htmlData)
+}
+
+func parseContent(input []byte) []byte {
+	// Parse the markdown file through blackfriday and bluemonday
+	// to generate a valid and safe HTML
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	// Create a buffer of bytes to write to file
+	var buffer bytes.Buffer
+	// Write html to bytes buffer
+	buffer.WriteString(header)
+	buffer.Write(body)
+	buffer.WriteString(footer)
+
+	return buffer.Bytes()
+}
+
+func saveHtml(outFilename string, data []byte) error {
+	// Write the bytes to the file
+	return ioutil.WriteFile(outFilename, data, filemode)
+}
---
+++ rggo/03.workingFiles/02.mdp/main_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil" // To read data from files
+	"os"
+	"strings"
+	"testing"
+)
+
+const (
+	inputFile  = "./testdata/test1.md"
+	goldenFile = "./testdata/test1.md.html"
+)
+
+func TestParseContent(t *testing.T) {
+	// Arrange
+	input, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Act
+	actual := parseContent(input)
+
+	// Assert
+	assertBytes(expected, actual, t)
+}
+
+func TestRun(t *testing.T) {
+	// Arrange
+	var mockStdOut bytes.Buffer
+
+	// Act
+	if err := run(inputFile, &mockStdOut); err != nil {
+		t.Fatal(err)
+	}
+
+	resultFile := strings.TrimSpace(mockStdOut.String())
+	exists, err := exists(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		defer os.Remove(resultFile)
+	}
+
+	actual, err := ioutil.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Assert
+	assertBytes(expected, actual, t)
+}
+
+func assertBytes(expected []byte, actual []byte, t *testing.T) {
+	if bytes.Equal(expected, actual) {
+		return
+	}
+
+	t.Logf("expected:\n%s\n", expected)
+	t.Logf("result:\n%s\n", actual)
+	t.Error("Actual content does not match expected")
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
+}
