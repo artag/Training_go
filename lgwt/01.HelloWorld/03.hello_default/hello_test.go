af5b404b50ebb556f1f218a8dd0a147f81d0e688
Artem Agafonov <agart@mail.ru>
2023-10-23 21:00:59 +0300

    lgwt. Chapter 1. Add examples.

---
+++ lgwt/01.HelloWorld/03.hello_default/hello_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	assert := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("\ngot: %q\nwant: %q\n", got, want)
+		}
+	}
+
+	t.Run(
+		"saying hello to people",
+		func(t *testing.T) {
+			got := Hello("Chris")
+			want := "Hello, Chris"
+			assert(t, got, want)
+		})
+
+	t.Run(
+		"say 'Hello, World' when an empty string is supplied",
+		func(t *testing.T) {
+			got := Hello("")
+			want := "Hello, World"
+			assert(t, got, want)
+		})
+}
