2d6ccdec333b761965dfac8ce5dcfab17025341b
Artem Agafonov <agart@mail.ru>
2023-11-29 21:52:56 +0300

    rggo. Chapter 6. Add goci version 6.

---
+++ rggo/06.processes/06.goci/errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors" // To define error values
+	"fmt"    // To format messages
+)
+
+var (
+	ErrValidation = errors.New("Validation failed")
+)
+
+type stepErr struct {
+	// Step error name.
+	step string
+	// Error message.
+	msg string
+	// Error.
+	cause error
+}
+
+func (s *stepErr) Error() string {
+	return fmt.Sprintf("Step: %q: %s: Cause: %v", s.step, s.msg, s.cause)
+}
+
+func (s *stepErr) Is(target error) bool {
+	t, ok := target.(*stepErr)
+	if !ok {
+		return false
+	}
+
+	return t.step == s.step
+}
+
+func (s *stepErr) Unwrap() error {
+	return s.cause
+}
