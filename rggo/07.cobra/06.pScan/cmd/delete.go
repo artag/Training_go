4f4e8de86f0ca182ab7dc3799b4cd428e13b7533
Artem Agafonov <agart@mail.ru>
2024-02-18 17:40:24 +0300

    rggo. Chapter 7. Add pScan version 6.

---
+++ rggo/07.cobra/06.pScan/cmd/delete.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt" // To use format fmt.Fprintln
+	"io"  // To use io.Writer interface
+	"os"  // To use os.Stdout for outputs
+	"rggo/cobra/pScan/scan"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:          "delete <host1>...<hostn>",
+	Aliases:      []string{"d"},
+	Short:        "Delete host(s) from list",
+	SilenceUsage: true,
+	Args:         cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile := viper.GetString("hosts-file")
+		return deleteAction(os.Stdout, hostsFile, args)
+	},
+}
+
+func init() {
+	hostsCmd.AddCommand(deleteCmd)
+}
+
+func deleteAction(out io.Writer, hostsFile string, args []string) error {
+	hl := &scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	for _, h := range args {
+		if err := hl.Remove(h); err != nil {
+			return err
+		}
+
+		fmt.Fprintln(out, "Deleted host:", h)
+	}
+
+	return hl.Save(hostsFile)
+}
