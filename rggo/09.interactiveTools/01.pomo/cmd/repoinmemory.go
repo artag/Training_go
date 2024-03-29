6956bc0fba14fa2d52ddbf62a6b4459917a14360
Artem Agafonov <agart@mail.ru>
2024-03-29 23:44:15 +0300

    rggo. Chapter 9. Add flags.

---
+++ rggo/09.interactiveTools/01.pomo/cmd/repoinmemory.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"rggo/interactiveTools/pomo/pomodoro"
+	"rggo/interactiveTools/pomo/pomodoro/repository"
+)
+
+func getRepo() (pomodoro.Repository, error) {
+	return repository.NewInMemoryRepo(), nil
+}
---
+++ rggo/09.interactiveTools/01.pomo/cmd/root.go
@@ -0,0 +1,92 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"rggo/interactiveTools/pomo/app"
+	"rggo/interactiveTools/pomo/pomodoro"
+	"time"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "pomo",
+	Short: "Interactive Pomodoro Timer",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		repo, err := getRepo()
+		if err != nil {
+			return err
+		}
+
+		config := pomodoro.NewConfig(
+			repo,
+			viper.GetDuration("pomo"),
+			viper.GetDuration("short"),
+			viper.GetDuration("long"),
+		)
+		return rootAction(config)
+	},
+}
+
+func rootAction(config *pomodoro.IntervalConfig) error {
+	a, err := app.New(config)
+	if err != nil {
+		return err
+	}
+
+	return a.Run()
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile, "config", "", "config file (default is $HOME/.pomo.yaml)")
+
+	rootCmd.Flags().DurationP("pomo", "p", 25*time.Minute, "Pomodoro duration")
+	rootCmd.Flags().DurationP("short", "s", 5*time.Minute, "Short break duration")
+	rootCmd.Flags().DurationP("long", "l", 15*time.Minute, "Long break duration")
+
+	viper.BindPFlag("pomo", rootCmd.Flags().Lookup("pomo"))
+	viper.BindPFlag("short", rootCmd.Flags().Lookup("short"))
+	viper.BindPFlag("long", rootCmd.Flags().Lookup("long"))
+}
+
+// Uses the package "viper" to include configuration management for application.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".pomo" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".pomo")
+	}
+
+	// Read in environment variables that match
+	viper.AutomaticEnv()
+
+	// If config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
