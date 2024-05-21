14cc0d14e106b1a59263ad7b833598b1448a6d7b
Artem Agafonov <agart@mail.ru>
2024-05-21 23:26:18 +0300

    Add html templating and inheritance.

---
+++ letsgo/02.Chapter/08.html_templating_inheritance/cmd/web/main.go
@@ -0,0 +1,21 @@
+// To test:
+// curl -i -X GET http://localhost:4000/snippet/view?id=123
+// curl -i -X POST http://localhost:4000/snippet/create
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
